5745101f99e0fcf7ab7bb6d0c77b36b0b1ddb505
LeonardoRauco <leo.rauco65@outlook.it>
2024-02-28 18:35:34 +0100

    esercitazione1fatta

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+// traccia 1
+//$nome = 'leonardo';
+//$is_student = true;
+//$float = 1.80;
+//$age = 23;
+//echo 'ciao sono ' . $nome . '' . ' sono uno studente' . ' ho ' . $age . ' anni ' . ' sono alto ' . $float . ' cm';
+
+//echo gettype($nome);
+//echo gettype($age);
+//var_dump ($is_student);
+//echo gettype($float);
+
+//const NOME = 'luca';
+//var_dump($NOME);
+
+//$nome = 'leo';
+
+//Traccia2 
+
+//$text1 = 'marco';
+//$text2 = 'hai';
+//$text3 = 'sete';
+//$text4 = '?';
+//$text5 = 'perchè';
+//$text6 = 'hai';
+//$text7 = 'bevuto';
+//$text8 = 'tutto';
+
+//echo $text1 . " " . $text2 . " " . $text3 . " " .$text4 ;
+
+//echo $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+
+
+$words1 = [
+    'una',
+    67,
+    'vita',
+    'colle',
+    'mi',
+    'rosso',
+    [
+    'oscura',
+    'era',
+    89,
+    [
+    'mezzo',
+    [
+    'cammin',
+    'Nel',
+    [
+    'selva',
+    'la',
+    [
+    'via',
+    'una',
+    true,
+    ]
+    ],
+    ]
+    ],
+    'ritrovai',
+    'per'
+    ],
+    'diritta'
+    ];
+    $words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'nostra',
+    'elemento3' => [
+    'Virgilio',
+    'smarrita',
+    'ché'
+    ]
+    ];
