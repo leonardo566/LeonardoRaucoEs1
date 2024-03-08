e98475353deab9463d2cdcdc1695e64ffea874ed
leonardo566 <147409155+leonardo566@users.noreply.github.com>
2024-03-08 18:16:05 +0100

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,55 @@
+<?php
+//TRACCIA 1 ES:29/02
+
+//$users = [
+   // ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'sig'],
+   // ];
+
+    //print_r($users);
+
+   // foreach($users as $elemento) {
+      //  echo " buongiorno" . " " . $elemento['gender'] . " " . $elemento['name'] . " " . $elemento['surname'] . "\n";
+    //}
+
+
+//TRACCIA 2
+// for each
+// $arrayNumerico = [1,8,3,16,5,12,7,8,9,14];
+// $somma = 0;
+// $counter = 0;
+ //foreach ($arrayNumerico as $numero) {
+   // if ($numero % 2 == 0) {
+       // $somma += $numero;
+        //$counter++;
+   // }
+// }
+
+// if($counter > 0) {
+   // $mediaNumeriPari = $somma / $conteggio ;
+    //echo "la media dei numeri pari è : $mediaNumeriPari\n";
+// }
+
+// ciclo for
+// $arrayNumerico = [1,8,3,16,5,12,7,8,9,14];
+// $somma = 0;
+// $conteggio = 0;
+// for ( $i = 0; $i < count($arrayNumerico);$i++) {
+//     if($arrayNumerico [$i] % 2 == 0 ){
+//         $somma += $arrayNumerico [$i];
+//         $conteggio++;
+
+//     }
+// }
+
+// if ($conteggio > 0) {
+//     $mediaNumeriPari = $somma / $conteggio ;
+//     echo "La media dei numeri pari è : $mediaNumeriPari\n";
+// }
+ 
+ 
+
+
+
+
+
+
